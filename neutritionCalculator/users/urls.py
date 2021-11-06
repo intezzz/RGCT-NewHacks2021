d04c18f7360d9be0e3ab9ed3a0726fc49c6715bc
gr4ce <wygrace.li@mail.utoronto.ca>
2021-11-06 16:05:34 -0400

    file structure change

--- neutritionCalculator/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rest_framework.authtoken.views import obtain_auth_token
-from django.contrib.auth import views as auth_views
-from .views import *
-
-urlpatterns = [
-    path('login/', obtain_auth_token, name="user-login"),
-    path('register/', register, name="user-register"),
-    path('is_admin/', is_admin, name="user-is-admin")]
