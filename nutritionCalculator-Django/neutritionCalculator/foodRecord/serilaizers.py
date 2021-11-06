2fc4ea6c1d461ed62cdcb913bc7e778d1cb11a95
gr4ce <wygrace.li@mail.utoronto.ca>
2021-11-06 16:10:12 -0400

    file structure update

--- nutritionCalculator-Django/neutritionCalculator/foodRecord/serilaizers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import FoodRecord
-
-
-class FoodRecordSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = FoodRecord
-        fields = ["id", "food_id", "quantity", "calories"]
